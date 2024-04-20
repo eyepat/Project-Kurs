96fe29e5d5fe760be68e12b6c4062d8d84d189c2
DominikMMateescu <dmmat@kth.se>
2024-04-20 19:07:02 +0200

    Fixade ett fel i controller.h och controller.c som gjorde att det inte gick att bygga ihop programmet

--- include/controller.h
+++ include/controller.h
@@ -5,6 +5,6 @@
 #include "model.h"
 
 // Function declarations for handling user input and events
-void handleEvents(bool *closeWindow, bool *up, bool *down, bool *left, bool *right);
+void handleEvents(bool *closeWindow, bool *up, bool *down, bool *left, bool *right, bool *up2, bool *down2, bool *left2, bool *right2);
 
 #endif /* CONTROLLER_H */
